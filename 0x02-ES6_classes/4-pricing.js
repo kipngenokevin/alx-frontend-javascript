bd8f04033d4bae92dc4d65af23516e407a79323e
kipngeno <kipngenokevin254@gmail.com>
2024-04-26 22:12:56 +0000

    Importing classess

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -7,16 +7,30 @@ export default class Pricing {
     this._currency = currency;
   }
 
-  get amount() { return this._amount; }
+  get amount() {
+    return this._amount;
+  }
 
-  get currency() { return this._currency; }
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw TypeError('amount must be a Number');
+    }
+    this._amount = amount;
+  }
 
-  set amount(amount) { this._amount = amount; }
+  get currency() {
+    return this._currency;
+  }
 
-  set currency(currency) { this._currency = currency; }
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw TypeError('currency must be a Currency');
+    }
+    this._currency = currency;
+  }
 
-  displayFullprice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
