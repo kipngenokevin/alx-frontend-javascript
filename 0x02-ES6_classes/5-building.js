5223499c2704c3a4cc56c57602b564182ac5196f
kipngeno <kipngenokevin254@gmail.com>
2024-04-27 18:47:16 +0000

    refactored the abstract class code

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw TypeError('Sqft must be a number');
-    }
-    this._sqft = sqft;
-    if (this.evacuationWarningMessage === undefined) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() { return this._sqft; }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw TypeError('Sqft must be a number');
-    }
-    this._sqft = sqft;
-  }
 }
