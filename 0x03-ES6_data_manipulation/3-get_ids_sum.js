6f75fd99cef2d1342a51357478624700ad752fad
kipngeno <kipngenokevin254@gmail.com>
2024-04-30 12:34:12 +0000

    function getListStudentIds that returns an array of ids from a list of object

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(students) {
-  const sumIds = students.reduce((accumulator, student) => student.id + student.id, 0);
+  const sumIds = students.reduce((accumulator, student) => accumulator + student.id, 0);
   return sumIds;
 }
