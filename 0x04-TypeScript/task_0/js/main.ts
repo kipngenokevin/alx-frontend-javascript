07585f28b4314efc8e21aed1454a671fd8ccb124
kipngeno <kipngenokevin254@gmail.com>
2024-05-04 11:45:03 +0000

    updated my typescript code

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -11,17 +11,17 @@ const student1: Student = {
   age: 25,
   location: 'Nairobi'
 };
-const student2:Student = {
+const student2: Student = {
   firstName: 'Brian',
   lastName: 'Kipkorir',
   age: 18,
   location: 'Nairobi'
 };
 
-const studentList:Student[] = [student1, student2];
+const studentList: Student[] = [student1, student2];
 
-function createTable(studentList) {
-  const studentTable = document.createElement("TABLE");
+function createTable(studentList: Student[]) {
+  const studentTable = document.createElement("TABLE") as HTMLTableElement;
 
   for (let i = 0; i < studentList.length; i = i + 1) {
     let row = studentTable.insertRow(i);
